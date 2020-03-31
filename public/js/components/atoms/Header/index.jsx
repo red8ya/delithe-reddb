2da0b62a30c59c1d036429986fb424a3ebcb5894
ハチヤ <hachiya.reddb@gmail.com>
2020-03-31 11:00:40 +0900

    Replace section header images by CSS ones.

--- public/js/components/atoms/Header/index.jsx
+++ public/js/components/atoms/Header/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styles from './styles.scss';
 
+const HeaderIcon = () => (
+  <span className={styles.headerIcon}>
+    <span className={styles.border1}></span>
+    <span className={styles.border2}></span>
+    <span className={styles.border3}></span>
+    <span className={styles.border4}></span>
+  </span>
+);
+
 const Header = ({children}) => (
   <div className={styles.header}>
-    <img src="/images/header-icon.png" />
+    <HeaderIcon />
     {children}
   </div>
 );
