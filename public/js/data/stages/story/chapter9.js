006d3a3d3e9ab0388020951a4f0578d83cb1fdd3
ハチヤ <hachiya.reddb@gmail.com>
2020-05-08 15:40:11 +0900

    Add several stages of Chapter 9 Episode 2.

--- public/js/data/stages/story/chapter9.js
+++ public/js/data/stages/story/chapter9.js
@@ -1,11 +1,14 @@
 import info from '../../../../data/stages/story/chapter9/info.yaml';
 import episode1Stages from '../../../../data/stages/story/chapter9/episode1/stages.yaml';
 import episode1Monsters from '../../../../data/stages/story/chapter9/episode1/monsters.yaml';
+import episode2Stages from '../../../../data/stages/story/chapter9/episode2/stages.yaml';
+import episode2Monsters from '../../../../data/stages/story/chapter9/episode2/monsters.yaml';
 import { mergeMonsters } from '../../utils';
 
 const episodes =
   [
-    [episode1Stages, episode1Monsters]
+    [episode1Stages, episode1Monsters],
+    [episode2Stages, episode2Monsters]
   ].map(([episodes, monsters]) => (
     episodes.map(episode => (
       Object.assign(episode, {
