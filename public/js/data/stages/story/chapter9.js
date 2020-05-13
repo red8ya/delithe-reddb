04a4eb863688af43900fe70e290e343b6b155b57
ハチヤ <hachiya.reddb@gmail.com>
2020-05-13 10:47:50 +0900

    Add missing stages of Chapter 9.

--- public/js/data/stages/story/chapter9.js
+++ public/js/data/stages/story/chapter9.js
@@ -3,12 +3,15 @@ import episode1Stages from '../../../../data/stages/story/chapter9/episode1/stag
 import episode1Monsters from '../../../../data/stages/story/chapter9/episode1/monsters.yaml';
 import episode2Stages from '../../../../data/stages/story/chapter9/episode2/stages.yaml';
 import episode2Monsters from '../../../../data/stages/story/chapter9/episode2/monsters.yaml';
+import episode3Stages from '../../../../data/stages/story/chapter9/episode3/stages.yaml';
+import episode3Monsters from '../../../../data/stages/story/chapter9/episode3/monsters.yaml';
 import { mergeMonsters } from '../../utils';
 
 const episodes =
   [
     [episode1Stages, episode1Monsters],
-    [episode2Stages, episode2Monsters]
+    [episode2Stages, episode2Monsters],
+    [episode3Stages, episode3Monsters]
   ].map(([episodes, monsters]) => (
     episodes.map(episode => (
       Object.assign(episode, {
