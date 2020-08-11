0a8c5a664ca9f9e1319d554a170f7676e8ff38c0
ハチヤ <hachiya.reddb@gmail.com>
2020-08-11 11:45:53 +0900

    Add Chapter 10 and the new abyss stages of 'フェルレーンの浮島'.

--- public/js/data/stages/story/index.js
+++ public/js/data/stages/story/index.js
@@ -7,7 +7,8 @@ import chapter6 from './chapter6';
 import chapter7 from './chapter7';
 import chapter8 from './chapter8';
 import chapter9 from './chapter9';
+import chapter10 from './chapter10';
 
-const story = [chapter1, chapter2, chapter3, chapter4, chapter5, chapter6, chapter7, chapter8, chapter9];
+const story = [chapter1, chapter2, chapter3, chapter4, chapter5, chapter6, chapter7, chapter8, chapter9, chapter10];
 
 export default story;
