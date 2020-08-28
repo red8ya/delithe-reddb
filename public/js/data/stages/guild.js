d538f5f9b772e26830d82e4d3113c60e7cfd6c19
ハチヤ <hachiya.reddb@gmail.com>
2020-08-28 10:23:02 +0900

    Add new guild dungeon stages of 'パーガトリーの実験場'.

--- public/js/data/stages/guild.js
+++ public/js/data/stages/guild.js
@@ -2,10 +2,13 @@ import pazuzuInfo from '../../../data/stages/guild/pazuzu/info.yaml';
 import pazuzuStages from '../../../data/stages/guild/pazuzu/stages.yaml';
 import needshegInfo from '../../../data/stages/guild/needsheg/info.yaml';
 import needshegStages from '../../../data/stages/guild/needsheg/stages.yaml';
+import purgatoryInfo from '../../../data/stages/guild/purgatory/info.yaml';
+import purgatoryStages from '../../../data/stages/guild/purgatory/stages.yaml';
 
 const guild = [
   Object.assign({}, pazuzuInfo, { episodes: pazuzuStages }),
-  Object.assign({}, needshegInfo, { episodes: needshegStages })
+  Object.assign({}, needshegInfo, { episodes: needshegStages }),
+  Object.assign({}, purgatoryInfo, { episodes: purgatoryStages }),
 ];
 
 export default guild;
