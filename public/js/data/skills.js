097d1a93affd4769969f627ea68cb06d784347dd
ハチヤ <hachiya.reddb@gmail.com>
2021-02-12 14:34:14 +0900

    Add a new job "Dark Blade".

--- public/js/data/skills.js
+++ public/js/data/skills.js
@@ -5,6 +5,7 @@ import ranger from '../../data/skills/ranger.yaml';
 import runeknight from '../../data/skills/runeknight.yaml';
 import paladin from '../../data/skills/paladin.yaml';
 import hermit from '../../data/skills/hermit.yaml';
+import darkblade from '../../data/skills/darkblade.yaml';
 import minstrel from '../../data/skills/minstrel.yaml';
 
 export const jobNames = {
@@ -15,11 +16,12 @@ export const jobNames = {
   runeknight: "ルーンナイト",
   paladin: "パラディン",
   hermit: "ハーミット",
+  darkblade: "ダークブレイド",
   minstrel: "ミンストレル",
 };
 
-export const jobs = ['knight', 'bishop', 'wizard', 'ranger', 'runeknight', 'paladin', 'hermit', 'minstrel'];
+export const jobs = ['knight', 'bishop', 'wizard', 'ranger', 'runeknight', 'paladin', 'hermit', 'darkblade', 'minstrel'];
 
-export const skills = { knight, bishop, wizard, ranger, runeknight, paladin, hermit, minstrel };
+export const skills = { knight, bishop, wizard, ranger, runeknight, paladin, hermit, darkblade, minstrel };
 
 export default skills;
